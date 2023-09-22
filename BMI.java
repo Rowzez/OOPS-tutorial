984a6b060f9a9c76f554e5a0eb3927cafc1442a8
Rowzez <145185305+Rowzez@users.noreply.github.com>
2023-09-22 02:21:17 -0700

    Update BMI.java

--- BMI.java
+++ BMI.java
@@ -5,7 +5,7 @@ public class BMI {
 
 	public static void main(String[] args) {
 		Scanner weight= new Scanner(System.in);
-		Scanner height= new Scanner(System.in);
+		Scanner height= new Scanner(System.in);//live and learn
 		
 		float w;
 		float h;
