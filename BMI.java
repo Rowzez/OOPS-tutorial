ad7f9299f1b87bc3399386d21e4e4303ad815657
Raphael <113975698+Raphael-pix@users.noreply.github.com>
2023-09-22 12:07:59 +0300

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,26 @@
+package net.javacode;
+import java.util.Scanner;
+
+public class BMI {
+
+	public static void main(String[] args) {
+		Scanner weight= new Scanner(System.in);
+		Scanner height= new Scanner(System.in);
+		
+		float w;
+		float h;
+		float bmi;
+		
+		System.out.print("Enter weight (in Kgs):");
+		w = weight.nextFloat();
+		
+		System.out.print("Enter height (in m):");
+		h = height.nextFloat();
+		
+		bmi = w/ (h*h);
+		
+		System.out.println("Your BMI is :" + bmi);
+
+	}
+
+}
